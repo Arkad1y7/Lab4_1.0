1f00bb6481458500da1ea0e3ad5d97b31f75c1a5
Arkady <arkadiy3344@yandex.ru>
2024-03-19 15:25:16 +0300

    update

---
+++ src/test/java/com/example/lab4/MainTest.java
@@ -0,0 +1,35 @@
+package com.example.lab4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
+public class MainTest {
+    private static LoginPage loginPage;
+    private static WebDriver browser;
+    @Before
+    public void setup() {
+        browser = new ChromeDriver();
+        browser.manage().window().maximize();
+        browser.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        browser.get(ConfProperties.getProperty("loginpage"));
+
+    }
+    @Test
+    public void loginTest() {
+        loginPage = new LoginPage(browser);
+        loginPage.clickPersonalArea();
+        loginPage.inputLogin("test");
+        loginPage.inputPassword("test");
+        loginPage.clickLoginButton();
+        loginPage.errorChecking();
+    }
+
+    @After
+    public void tearDown() {
+        browser.quit();
+    }
+}
